f6e02efcb034e1eba1885c03c091eb0d851e8cc8
Anders Lima <anders.lima@sr-bank.no>
2024-02-27 12:14:47 +0100

    Updated progress bar

--- InstallStuff.ps1
+++ InstallStuff.ps1
@@ -5,42 +5,60 @@
 "Greenshot.Greenshot",
 "JetBrains.Toolbox",
 "JetBrains.Rider",
-"JetBrains.IntelliJIDEA.Ultimate",
 "Spotify.Spotify",
 "SlackTechnologies.Slack", 
 "Docker.DockerDesktop",
 "Logitech.Options",
 "VivaldiTechnologies.Vivaldi",
-"Microsoft.WindowsTerminal"
+"Microsoft.WindowsTerminal",
 "Microsoft.DotNet.SDK.7",
 "Microsoft.DotNet.SDK.8",
 "Microsoft.VisualstudioCode", 
 "Microsoft.AzureDataStudio",
 "Microsoft.AzureCLI",
+"Microsoft.PowerShell"
 "Microsoft.NuGet",
 "JanDeDobbeleer.OhMyPosh",
 "9NMPJ99VJBWV" # Windows Phone Link
 )
 
 $totalApps = $apps.Count
+$jobs = @()
 For ($i = 0; $i -lt $totalApps; $i++) {
     $app = $apps[$i]
-    $activity = "InstallStuff:"
     $progress = 100*$i/$totalApps
-    Write-Progress $activity -Id 0 -Status  "$app : Checking if already installed..." -PercentComplete $progress
+    $activity = "Installing $app"
+    Write-Progress -Activity "Installing apps..." -Id 999 -Status "Installing $app..." -PercentComplete $progress
+    Write-Progress -Activity $activity -Id $i -ParentId 999 -Status  "Checking if $app is already installed..." -PercentComplete 0
 
-    if($(winget list -q $app | Measure-Object -Line).Lines -lt 5){
-        Write-Progress $activity -Id 0 -Status "$app : Installing... " -PercentComplete ($progress+1)
-        Start-Job -Name $app -ScriptBlock { winget -ArgumentList install $app --accept-package-agreements --silent}
+    if((winget list | Select-String -Pattern $app -Quiet) -eq $true){
+        Write-Progress -Activity $activity -Id $i -ParentId 999  -Status  "Already Present. Skipping." -PercentComplete 100 -Completed
+        $jobs += Start-Job -Name $app -ScriptBlock { Write-Information "Already Present. Skipping."}
     }else{
-        Write-Progress $activity -Id 0 -Status  "$app : Already Present. Skipping." -PercentComplete ($progress+1)
+        Write-Progress -Activity $activity -Id $i -ParentId 999  -Status "Installing... " -PercentComplete 10
+        $jobs += Start-Job -Name $app -ScriptBlock { winget install $args} -ArgumentList $app, --accept-package-agreements, --silent
     }
 }
-Write-Progress $activity -Id 0 -Status  "Waiting for installs to complete..." -PercentComplete (100)
+
 do {
-    $jobs = Get-Job | Format-Table -AutoSize
-    $jobs
-    Start-Sleep 10
-} while (
-     1 -eq 1
-)
+    $currentJobs = Get-Job
+    $completedJobs = ($currentJobs | Where-Object { $_.State -eq "Completed" }).Count
+    $progress = 100*$completedJobs/$totalApps
+    for ($i = 0; $i -lt $totalApps; $i++) {
+        $app = $apps[$i]
+        $activity = "Installing $app"
+        if ($currentJobs[$i].State -eq "Completed") {
+            Write-Progress -Activity $activity -Id $i -ParentId 999 -Status  "Installation Complete." -PercentComplete 100 -Completed
+        } else {
+            Write-Progress -Activity $activity -Id $i -ParentId 999 -Status  "Installing..." -PercentComplete 50
+        }
+    }
+    Write-Progress -Activity "Installing Apps" -Id 999 -Status  "Waiting for apps to complete..." -PercentComplete $progress
+    Start-Sleep -Seconds 2
+} while (($currentJobs | Where-Object { $_.State -eq "Completed" }).Count -lt $totalApps)
+Write-Progress -Activity "Installing Apps" -Id 999 -Status  "Installed $completedJobs apps" -PercentComplete 100
+Get-Job | Format-Table -AutoSize
+foreach ($job in $jobs) {
+    Receive-Job -Job $job
+    Remove-Job -Job $job
+}
